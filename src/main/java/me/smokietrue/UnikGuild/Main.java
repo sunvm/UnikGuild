d9a15ccda3b624c8e4e95492af208711d269f96c
sunvm <sakvt1@outlook.com>
2026-01-16 11:21:40 +0300

    fix

--- src/main/java/me/smokietrue/UnikGuild/Main.java
+++ src/main/java/me/smokietrue/UnikGuild/Main.java
@@ -10,19 +10,38 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Инициализация компонентов
         this.config = new Config(this);
         this.guildManager = new GuildManager(this);
         this.itemManager = new ItemManager(this);
         this.eventListener = new EventListener(this);
         
+        // Регистрация событий
         getServer().getPluginManager().registerEvents(eventListener, this);
         
-        // Регистрируем все команды
+        // Регистрируем команды С ПРОВЕРКОЙ на null
         GuildCommand guildCommand = new GuildCommand(this);
-        getCommand("guilds").setExecutor(guildCommand);
-        getCommand("g").setExecutor(guildCommand);
-        getCommand("guildcolor").setExecutor(guildCommand);
         
+        // Команда "guilds" (должна быть в plugin.yml)
+        if (getCommand("guilds") != null) {
+            getCommand("guilds").setExecutor(guildCommand);
+        } else {
+            getLogger().severe("Команда 'guilds' не найдена в plugin.yml! Плагин будет отключен.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        
+        // Команда "g" (должна быть в plugin.yml)
+        if (getCommand("g") != null) {
+            getCommand("g").setExecutor(guildCommand);
+        } else {
+            getLogger().warning("Команда 'g' не найдена в plugin.yml, но это не критично.");
+        }
+        
+        // КОМАНДА "guildcolor" - УБЕРИТЕ ЭТУ СТРОКУ ИЛИ ДОБАВЬТЕ В plugin.yml!
+        // getCommand("guildcolor").setExecutor(guildCommand); // ← ЭТУ СТРОКУ НУЖНО УДАЛИТЬ!
+        
+        // Загрузка данных
         guildManager.loadGuilds();
         
         getLogger().info("UnikGuild включен! Автор: smokietrue");
@@ -30,7 +49,9 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        guildManager.saveGuilds();
+        if (guildManager != null) {
+            guildManager.saveGuilds();
+        }
         getLogger().info("UnikGuild выключен!");
     }
 
